0660d718e3a04ae784ee9c83f524820d1fcf1a91
Paolo Viviani <pvi@localhost.localdomain>
2018-06-20 16:20:39 +0200

    cleaned up test dir

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -4,13 +4,14 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MXNET_LIBDIR
 
 export GAM_HOME=$(cd ..; pwd)/3rdparty/gam/gam
 export GAM_INCS=$GAM_HOME/include
-export GAM_RUN_LOCAL=./gamrun-local
+export GAM_RUN_LOCAL=./bin/gamrun-local
 export GAM_CONF=$GAM_HOME/conf/local.conf
 export GAM_LOCALHOST=localhost
 
 export GAM_RANK=0
-export GAM_CARDINALITY=1
 export GAM_LOG_PREFIX=
 
-$GAM_RUN_LOCAL -v -n 1 -l $GAM_LOCALHOST ./unit_test
-$GAM_RUN_LOCAL -v -n 2 -l $GAM_LOCALHOST ./gam_unit_test
+export GAM_CARDINALITY=1
+$GAM_RUN_LOCAL -v -n 1 -l $GAM_LOCALHOST ./bin/unit_test
+export GAM_CARDINALITY=2
+$GAM_RUN_LOCAL -v -n 2 -l $GAM_LOCALHOST ./bin/gam_unit_test
