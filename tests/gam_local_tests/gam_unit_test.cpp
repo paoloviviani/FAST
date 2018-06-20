bb056929a39cf7f1cd5f3877afd1bd2188a8d298
Paolo Viviani <pvi@localhost.localdomain>
2018-06-20 15:48:09 +0200

    gam tensor ping-pong is working

--- tests/gam_local_tests/gam_unit_test.cpp
+++ tests/gam_local_tests/gam_unit_test.cpp
@@ -62,48 +62,31 @@ TEST_CASE( "SPMD tensor send", "gam,tensor" ) {
 	}
 }
 
-//TEST_CASE( "SPMD tensor ping-pong", "gam,tensor" ) {
-//	if (gam::cardinality() > 1) {
-//		switch (gam::rank()) {
-//		case 0:
-//		{
-//			/* create a private pointer */
-//			auto p = gam::make_private<gam_vector<int>>();
-//
-//			/* populate */
-//			auto lp = p.local();
-//			lp->push_back(42);
-//
-//			/* push to 1 */
-//			p = gam::private_ptr<gam_vector<int>>(std::move(lp));
-//			p.push(1);
-//
-//			/* wait for the response */
-//			p = gam::pull_private<gam_vector<int>>(1);
-//
-//			/* test */
-//			lp = p.local();
-//			REQUIRE(lp->size() == 10);
-//			REQUIRE(lp->at(1) == 43);
-//			break;
-//		}
-//		case 1:
-//		{
-//			/* pull private pointer from 0 */
-//			auto p = gam::pull_private<gam_vector<int>>(); //from-any just for testing
-//
-//			/* test and add */
-//			auto lp = p.local();
-//			REQUIRE(lp->size() == 1);
-//			REQUIRE(lp->at(0) == 42);
-//			lp->push_back(43);
-//			lp->resize(10);
-//
-//			/* push back to 0 */
-//			p = gam::private_ptr<gam_vector<int>>(std::move(lp));
-//			p.push(0);
-//			break;
-//		}
-//		}
-//	}
-//}
+TEST_CASE( "SPMD tensor ping-pong", "gam,tensor" ) {
+	if (gam::cardinality() > 1) {
+		vector<unsigned int> shape = {2,3};
+		vector<float> data = {11.,12.,13.,21.,22.,23.};
+		switch (gam::rank()) {
+		case 0:
+		{
+			FAST::Tensor<NDArray> tensor(data.data(),shape);
+			tensor.push(1);
+			auto p = gam::pull_private<gam_vector<float>>(1);
+			auto p_local = p.local();
+			for (uint i = 0; i < shape[0]; i++)
+				for (uint j = 0; j < shape[1]; j++){
+					REQUIRE(p_local->at(i*shape[1]+j) == data.at(i*shape[1]+j));
+				}
+			break;
+		}
+		case 1:
+		{
+			auto p = gam::pull_private<gam_vector<float>>();
+			auto p_local = p.local();
+			FAST::Tensor<NDArray> tensor(p_local->data(),shape);
+			tensor.push(0);
+			break;
+		}
+		}
+	}
+}
