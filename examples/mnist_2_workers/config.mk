1a94c96d57fbf44b2e2b3d2d55f47ef1eb97dbcf
Paolo Viviani <paolo.viviani@unito.it>
2018-06-26 14:41:19 +0000

    sinc

--- examples/mnist_2_workers/config.mk
+++ examples/mnist_2_workers/config.mk
@@ -5,3 +5,4 @@ MXNET_INCLUDE_DIR		=	$(FAST_ROOT)/3rdparty/mxnet/include
 MXNET_LIB_DIR			=	/mxnet/lib
 LIBFABRIC_LIB_DIR		=	/opt/libfabric/lib
 LIBFABRIC_INCLUDE_DIR	=	/opt/libfabric/include
+USE_CUDA				=	1
