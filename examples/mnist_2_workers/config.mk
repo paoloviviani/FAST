333f09b5e341c0f7df83e970024b39bc9021a67d
Paolo Viviani <paolo.viviani@unito.it>
2018-06-26 13:26:38 +0000

    sync

--- examples/mnist_2_workers/config.mk
+++ examples/mnist_2_workers/config.mk
@@ -2,5 +2,5 @@ FAST_ROOT				=	../..
 #error, info, debug
 DEBUG					=	error
 MXNET_INCLUDE_DIR		=	$(FAST_ROOT)/3rdparty/mxnet/include
-MXNET_LIB_DIR			=	$(FAST_ROOT)/3rdparty/mxnet/lib
+MXNET_LIB_DIR			=	/mxnet/lib
 LIBFABRIC_LIB_DIR		=	
