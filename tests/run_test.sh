aa25220c95bb9773089d95d11beb429f562b888f
Paolo Viviani <pvi@localhost.localdomain>
2018-08-07 16:06:19 +0200

    minor

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -18,5 +18,5 @@ fi
 
 #$GAM_RUN_LOCAL -v -n 1 -l $GAM_LOCALHOST ./bin/unit_test
 #$GAM_RUN_LOCAL -v -n 2 -l $GAM_LOCALHOST ./bin/gam_unit_test
-$GAM_RUN_LOCAL -v -n 6 -l $GAM_LOCALHOST ./bin/gff_farm
+#$GAM_RUN_LOCAL -v -n 6 -l $GAM_LOCALHOST ./bin/gff_farm
 $GAM_RUN_LOCAL -v -n 4 -l $GAM_LOCALHOST ./bin/gff_all_reduce
