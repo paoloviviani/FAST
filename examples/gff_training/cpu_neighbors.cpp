e85d3dfdceba8e3cb545ad8245bf36d2c6f92e0a
Paolo Viviani <pvi@localhost.localdomain>
2018-08-08 18:40:29 +0200

    sync

--- examples/gff_training/cpu_neighbors.cpp
+++ examples/gff_training/cpu_neighbors.cpp
@@ -9,25 +9,25 @@ using namespace std;
 using namespace mxnet::cpp;
 
 Symbol mlp(const vector<int> &layers) {
-  auto x = Symbol::Variable("X");
-  auto label = Symbol::Variable("label");
+	auto x = Symbol::Variable("X");
+	auto label = Symbol::Variable("label");
 
-  vector<Symbol> weights(layers.size());
-  vector<Symbol> biases(layers.size());
-  vector<Symbol> outputs(layers.size());
+	vector<Symbol> weights(layers.size());
+	vector<Symbol> biases(layers.size());
+	vector<Symbol> outputs(layers.size());
 
-  for (size_t i = 0; i < layers.size(); ++i) {
-    weights[i] = Symbol::Variable("w" + to_string(i));
-    biases[i] = Symbol::Variable("b" + to_string(i));
-    Symbol fc = FullyConnected(
-      i == 0? x : outputs[i-1],  // data
-      weights[i],
-      biases[i],
-      layers[i]);
-    outputs[i] = i == layers.size()-1 ? fc : Activation(fc, ActivationActType::kRelu);
-  }
+	for (size_t i = 0; i < layers.size(); ++i) {
+		weights[i] = Symbol::Variable("w" + to_string(i));
+		biases[i] = Symbol::Variable("b" + to_string(i));
+		Symbol fc = FullyConnected(
+				i == 0? x : outputs[i-1],  // data
+						weights[i],
+						biases[i],
+						layers[i]);
+		outputs[i] = i == layers.size()-1 ? fc : Activation(fc, ActivationActType::kRelu);
+	}
 
-  return SoftmaxOutput(outputs.back(), label);
+	return SoftmaxOutput(outputs.back(), label);
 }
 
 class MXNetWorkerLogic {
@@ -35,29 +35,60 @@ public:
 
 	gff::token_t svc(gam::public_ptr< FAST::gam_vector<float> > &in, gff::NDOneToAll &c) {
 
-
-		buffer_.push_back(in);
-		if (buffer_.size() < 2)
-			return gff::go_on;
-		else {
-			int sum = std::accumulate(buffer_.begin(), buffer_.end(), 0);
-			return gff::eos;
-		}
 	}
 
 	void svc_init(gff::NDOneToAll &c) {
-		model_.init();
+		const int image_size = 28;
+		const vector<int> layers{128, 64, 10};
+		const int batch_size = 128;
+		const int max_epoch = 10;
+		const float learning_rate = 0.001;
+		const float weight_decay = 1e-4;
+
+		train_iter = MXDataIter("MNISTIter")
+			  .SetParam("image", "../data/mnist_data/train-images-idx3-ubyte")
+			  .SetParam("label", "../data/mnist_data/train-labels-idx1-ubyte")
+			  .SetParam("batch_size", batch_size)
+			  .SetParam("flat", 1)
+			  .CreateDataIter();
+
+		net = mlp(layers);
+
+		Context ctx = Context::cpu();  // Use CPU for training
+
+		args["X"] = NDArray(Shape(batch_size, image_size*image_size), ctx);
+		args["label"] = NDArray(Shape(batch_size), ctx);
+		// Let MXNet infer shapes other parameters such as weights
+		net.InferArgsMap(ctx, &args, args);
+
+		// Initialize all parameters with uniform distribution U(-0.01, 0.01)
+		auto initializer = Uniform(0.01);
+		for (auto& arg : args) {
+			// arg.first is parameter name, and arg.second is the value
+			initializer(arg.first, &arg.second);
+		}
+
+		opt = OptimizerRegistry::Find("adam");
+		opt->SetParam("rescale_grad", 1.0/batch_size)
+		    		 ->SetParam("lr", learning_rate)
+					 ->SetParam("wd", weight_decay);
+		exec = net.SimpleBind(ctx, args);
+		arg_names = net.ListArguments();
+
 	}
 
 	void svc_end() {
-		model_.finalize();
 	}
 private:
 	array<unsigned int,2> idx;
 	MXDataIter train_iter;
 	Symbol net;
 	std::map<string, NDArray> args;
+	Optimizer* opt;
 	Executor * exec;
+	vector<string> arg_names;
+	Accuracy train_acc;
+	unsigned int local_epoch;
 };
 
 
