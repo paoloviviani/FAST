d47ab02ac2bed500ce0c7877a14a7088358701c7
Paolo Viviani <paolo.vivi@gmail.com>
2018-09-14 16:45:10 +0200

    removed old tensor testst

--- tests/gam_tests/gam_unit_test.cpp
+++ tests/gam_tests/gam_unit_test.cpp
@@ -26,59 +26,6 @@ TEST_CASE( "Gam basic", "gam" ) {
 	REQUIRE(*local_p == 42);
 }
 
-TEST_CASE( "SPMD tensor send", "gam,tensor" ) {
-	if (gam::cardinality() > 1) {
-		vector<unsigned int> shape = {2,3};
-		vector<float> data = {11.,12.,13.,21.,22.,23.};
-		switch (gam::rank()) {
-		case 0:
-		{
-			FAST::Tensor<float> tensor(data.data(),shape);
-			tensor.push(1);
-			break;
-		}
-		case 1:
-		{
-			auto recv = FAST::pull_tensor<float>();
-			recv->reShape(shape);
-			for (uint i = 0; i < shape[0]; i++)
-				for (uint j = 0; j < shape[1]; j++){
-					REQUIRE(recv->at(i,j) == data.at(i*shape[1]+j));
-				}
-			break;
-		}
-		}
-	}
-}
-
-TEST_CASE( "SPMD tensor ping-pong", "gam,tensor" ) {
-	if (gam::cardinality() > 1) {
-		vector<unsigned int> shape = {2,3};
-		vector<float> data = {11.,12.,13.,21.,22.,23.};
-		switch (gam::rank()) {
-		case 0:
-		{
-			FAST::Tensor<float> tensor(data.data(),shape);
-			tensor.push(1);
-			auto recv = FAST::pull_tensor<float>();
-			recv->reShape(shape);
-			for (uint i = 0; i < shape[0]; i++)
-				for (uint j = 0; j < shape[1]; j++){
-					REQUIRE(recv->at(i,j) == data.at(i*shape[1]+j));
-				}
-			break;
-		}
-		case 1:
-		{
-			auto recv = FAST::pull_tensor<float>();
-			recv->reShape(shape);
-			recv->push(0);
-			break;
-		}
-		}
-	}
-}
-
 TEST_CASE( "SPMD public vector ping-pong", "gam,vector,public" ) {
 	if (gam::cardinality() > 1) {
 		vector<int> ref = {1,2,3};
