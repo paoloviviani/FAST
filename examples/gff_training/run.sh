5448d4a832c4fde409f6c01ad2193686df815614
Paolo Viviani <paolo.vivi@gmail.com>
2018-09-14 16:38:47 +0200

    fixed gamrun local

--- examples/gff_training/run.sh
+++ examples/gff_training/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 FAST_ROOT=$(cd ../..; pwd)
-MXNET_LIBDIR=/mxnet/lib
+MXNET_LIBDIR=$FAST_ROOT/3rdparty/mxnet/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MXNET_LIBDIR
 
 export GAM_HOME=$FAST_ROOT/3rdparty/gam/gam
@@ -11,7 +11,7 @@ export GAM_LOCALHOST=localhost
 export GAM_LOG_PREFIX=
 
 # Use no more than 2
-export GAM_CARDINALITY=2
+export GAM_CARDINALITY=3
 
 if [[ $* == *-l* ]]; then
 export GAM_RUN_LOCAL=$FAST_ROOT/bin/gamrun-local_logging
@@ -19,4 +19,4 @@ else
 export GAM_RUN_LOCAL=$FAST_ROOT/bin/gamrun-local
 fi
 
-$GAM_RUN_LOCAL -v -n $GAM_CARDINALITY -l $GAM_LOCALHOST ./mnist_cpu
+$GAM_RUN_LOCAL -v -n $GAM_CARDINALITY -l $GAM_LOCALHOST ./basic_gff_train
