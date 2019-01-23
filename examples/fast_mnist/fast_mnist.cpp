0bbf712579de1626cc08d010f826a3638f4e59ad
Paolo Viviani <paolo.vivi@gmail.com>
2019-01-24 00:29:07 +0100

    sync

--- examples/fast_mnist/fast_mnist.cpp
+++ examples/fast_mnist/fast_mnist.cpp
@@ -114,6 +114,7 @@ public:
 	}
 
 	void update(std::vector<mxnet::cpp::NDArray> &in) {
+		FAST_DEBUG("(LOGIC UPDATE): updating")
 		if (in.size() > 0) {
 			int ii = 0;
 			for (size_t i = 0; i < arg_names.size(); ++i) {
