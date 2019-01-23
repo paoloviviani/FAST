45a2de434114334f76f643feabaf5a3783bbf715
Paolo Viviani <paolo.vivi@gmail.com>
2019-01-24 00:37:58 +0100

    sync

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
-FAST_ROOT			=	$(dir $(realpath $(lastword $(MAKEFILE_LIST))))
+FAST_ROOT				=	$(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 #error, info, debug
-DEBUG				=	debug
+DEBUG					=	debug
 MXNET_LIB_DIR			=	$(FAST_ROOT)/3rdparty/mxnet/lib
-LIBFABRIC_LIB_DIR		=	
-USE_GAM				=	1
+LIBFABRIC_ROOT			=	
+USE_GAM					=	1
