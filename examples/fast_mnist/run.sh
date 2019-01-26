eef03f03c382e74026096892e839f7ee22c2bd8b
Paolo Viviani <paolo.viviani@unito.it>
2019-01-26 16:25:23 +0000

    run fats_mnist

--- examples/fast_mnist/run.sh
+++ examples/fast_mnist/run.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 FAST_ROOT=$(cd ../..; pwd)
 MXNET_LIBDIR=$FAST_ROOT/3rdparty/mxnet/lib
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MXNET_LIBDIR
+LIBFABRIC_ROOT=/opt/libfabric
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MXNET_LIBDIR:$LIBFABRIC_ROOT/lib
 
-export GAM_HOME=$FAST_ROOT/3rdparty/gam/gam
-export GAM_INCS=$GAM_HOME/include
-export GAM_CONF=$GAM_HOME/conf/local.conf
-export GAM_LOCALHOST=localhost
+#export GAM_HOME=$FAST_ROOT/3rdparty/gam/gam
+#export GAM_INCS=$GAM_HOME/include
+#export GAM_CONF=$GAM_HOME/conf/local.conf
+#export GAM_LOCALHOST=localhost
 
-export GAM_LOG_PREFIX=
+#export GAM_LOG_PREFIX=$PWD/logs
 
-export GAM_CARDINALITY=2
+#export GAM_CARDINALITY=2
 
-if [[ $* == *-l* ]]; then
-export GAM_RUN_LOCAL=$FAST_ROOT/bin/gamrun-local_logging
-else
-export GAM_RUN_LOCAL=$FAST_ROOT/bin/gamrun-local
-fi
+#if [[ $* == *-l* ]]; then
+#export GAM_RUN_LOCAL=$FAST_ROOT/bin/gamrun-local_logging
+#else
+export GAM_RUN_LOCAL=$FAST_ROOT/bin/fastrun-local
+#fi
 
-$GAM_RUN_LOCAL -v -n $GAM_CARDINALITY -l $GAM_LOCALHOST ./fast_mnist
+#$GAM_RUN_LOCAL -v -n $GAM_CARDINALITY -l localhost ./fast_mnist
+$FAST_ROOT/bin/fastrun-mpi -H hosts -n 2 $PWD/fast_mnist
