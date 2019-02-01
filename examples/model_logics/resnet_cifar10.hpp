431de9e87f7fd10cef45df09ae4bdb1368689e8f
Paolo Viviani <paolo.vivi@gmail.com>
2019-02-01 22:35:48 +0100

    added cifar10 example

--- examples/model_logics/resnet_cifar10.hpp
+++ examples/model_logics/resnet_cifar10.hpp
@@ -4,45 +4,30 @@
 
 using namespace mxnet::cpp;
 
-Symbol mlp(const std::vector<int> &layers) {
-	auto x = Symbol::Variable("X");
-	auto label = Symbol::Variable("label");
-
-	std::vector<Symbol> weights(layers.size());
-	std::vector<Symbol> biases(layers.size());
-	std::vector<Symbol> outputs(layers.size());
-
-	for (size_t i = 0; i < layers.size(); ++i) {
-		weights[i] = Symbol::Variable("w" + to_string(i));
-		biases[i] = Symbol::Variable("b" + to_string(i));
-		Symbol fc = FullyConnected(
-				i == 0? x : outputs[i-1],  // data
-						weights[i],
-						biases[i],
-						layers[i]);
-		outputs[i] = i == layers.size()-1 ? fc : Activation(fc, ActivationActType::kRelu);
-	}
-
-	return SoftmaxOutput(outputs.back(), label);
-}
+Context ctx = Context::cpu();  // Use CPU for training
 
 class ModelLogic {
 public:
 	void init() {
 		const int image_size = 28;
-		const std::vector<int> layers{256, 64, 32, 10};
 		batch_size_ = 32;
 		const float learning_rate = 0.001;
+		const float learning_rate = 0.01;
+		const float weight_decay = 1e-4;
 
-		net = mlp(layers);
+		auto net = Symbol::Load("./symbols/resnet18_v2.json");
 
-		Context ctx = Context::cpu();  // Use CPU for training
-
-		train_iter.SetParam("image", "../mnist_data/train-images-idx3-ubyte")
-			  .SetParam("label", "../mnist_data/train-labels-idx1-ubyte")
-			  .SetParam("batch_size", batch_size_)
-			  .SetParam("flat", 1)
-			  .CreateDataIter();
+		MXDataIter("ImageRecordIter")
+			.SetParam("path_imglist", "../cifar10/cifar10_train.lst")
+			.SetParam("path_imgrec", "../cifar10/cifar10_train.rec")
+			.SetParam("rand_crop", 1)
+			.SetParam("rand_mirror", 1)
+			.SetParam("data_shape", Shape(3, 32, 32))
+			.SetParam("batch_size", batch_size_)
+			.SetParam("shuffle", 1)
+			.SetParam("preprocess_threads", 24)
+			.SetParam("pad", 2)
+			.CreateDataIter();
 
 		args["X"] = NDArray(Shape(batch_size_, image_size*image_size), ctx);
 		args["label"] = NDArray(Shape(batch_size_), ctx);
@@ -74,17 +59,14 @@ public:
 			epoch_++;
 			FAST_INFO("=== TRAINING ACCURACY === " << train_acc.Get());
 			train_iter.Reset();
-		    train_acc.Reset();
+			train_acc.Reset();
 		}
 
-		if (epoch_ == 10){
+		if (epoch_ == max_epoch_){
 			FAST_DEBUG("(LOGIC): MAX EPOCH REACHED");
 			max_epoch_reached = true; // Terminate
 			return;
 		}
-
-		// Simulate granularity
-		std::this_thread::sleep_for(std::chrono::milliseconds(300));
 
 		auto data_batch = train_iter.GetDataBatch();
 		// Set data and label
@@ -107,35 +89,41 @@ public:
 
 	void update(std::vector<mxnet::cpp::NDArray> &in) {
 		FAST_DEBUG("(LOGIC UPDATE): updating")
-		if (in.size() > 0) {
-			int ii = 0;
-			for (size_t i = 0; i < arg_names.size(); ++i) {
-				if (arg_names[i] == "X" || arg_names[i] == "label") continue;
-				opt->Update(i, exec->arg_arrays[i], in[ii]);
-				ii++;
-			}
-			FAST_DEBUG("(LOGIC UPDATE): updated")
-		}
+				if (in.size() > 0) {
+					int ii = 0;
+					for (size_t i = 0; i < arg_names.size(); ++i) {
+						if (arg_names[i] == "X" || arg_names[i] == "label") continue;
+						opt->Update(i, exec->arg_arrays[i], in[ii]);
+						ii++;
+					}
+					FAST_DEBUG("(LOGIC UPDATE): updated")
+				}
 	}
 
 	void finalize() {
-		  auto val_iter = MXDataIter("MNISTIter")
-		      .SetParam("image", "../data/mnist_data/t10k-images-idx3-ubyte")
-		      .SetParam("label", "../data/mnist_data/t10k-labels-idx1-ubyte")
-			  .SetParam("batch_size", batch_size_)
-			  .SetParam("flat", 1)
-			  .CreateDataIter();
-		  Accuracy acc;
-		    val_iter.Reset();
-		    while (val_iter.Next()) {
-		  	auto data_batch = val_iter.GetDataBatch();
-		  	data_batch.data.CopyTo(&args["X"]);
-		  	data_batch.label.CopyTo(&args["label"]);
-		  	// Forward pass is enough as no gradient is needed when evaluating
-		  	exec->Forward(false);
-		  	acc.Update(data_batch.label, exec->outputs[0]);
-		    }
-		    FAST_INFO("=== VALIDATION ACCURACY === " << train_acc.Get());
+		auto val_iter = MXDataIter("ImageRecordIter")
+			.SetParam("path_imglist", "../cifar10/cifar10_val.lst")
+			.SetParam("path_imgrec", "../cifar10/cifar10_val.rec")
+			.SetParam("rand_crop", 0)
+			.SetParam("rand_mirror", 0)
+			.SetParam("data_shape", Shape(3, 32, 32))
+			.SetParam("batch_size", batch_size_)
+			.SetParam("round_batch", 0)
+			.SetParam("preprocess_threads", 24)
+			.SetParam("pad", 2)
+			.CreateDataIter();
+
+		Accuracy acc;
+		val_iter.Reset();
+		while (val_iter.Next()) {
+			auto data_batch = val_iter.GetDataBatch();
+			data_batch.data.CopyTo(&args["X"]);
+			data_batch.label.CopyTo(&args["label"]);
+			// Forward pass is enough as no gradient is needed when evaluating
+			exec->Forward(false);
+			acc.Update(data_batch.label, exec->outputs[0]);
+		}
+		FAST_INFO("=== VALIDATION ACCURACY === " << train_acc.Get());
 	}
 
 	Symbol net;
@@ -149,4 +137,5 @@ public:
 	MXDataIter train_iter = MXDataIter("MNISTIter");
 	Accuracy train_acc;
 	int batch_size_ = 32;
+	const int max_epoch_ = 200;
 };
