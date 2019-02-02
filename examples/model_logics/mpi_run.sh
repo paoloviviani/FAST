a9fd885b79f3230b64bbf4621b6bbd7f2cde3c58
Paolo Viviani <paolo.vivi@gmail.com>
2019-02-02 15:25:22 +0100

    sync

--- examples/model_logics/mpi_run.sh
+++ examples/model_logics/mpi_run.sh
@@ -4,7 +4,7 @@ MXNET_LIBDIR=$FAST_ROOT/3rdparty/mxnet/lib
 LIBFABRIC_ROOT=/opt/libfabric
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MXNET_LIBDIR:$LIBFABRIC_ROOT/lib
 
-export GAM_CARDINALITY=2
+export GAM_CARDINALITY=16
 export GAM_RUN_LOCAL=$FAST_ROOT/bin/fastrun-local
 
-$FAST_ROOT/bin/fastrun-mpi -H hosts -n $GAM_CARDINALITY "$@"
+$FAST_ROOT/bin/fastrun-mpi -H hosts -n $GAM_CARDINALITY "$1 4 4"
