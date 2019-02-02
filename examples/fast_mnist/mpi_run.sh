6760faecd721e960480995a1b2c30e9eab3dddf5
Paolo Viviani <paolo.vivi@gmail.com>
2019-02-02 14:48:39 +0100

    sync

---
+++ examples/fast_mnist/mpi_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+FAST_ROOT=$(cd ../..; pwd)
+MXNET_LIBDIR=$FAST_ROOT/3rdparty/mxnet/lib
+LIBFABRIC_ROOT=/opt/libfabric
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MXNET_LIBDIR:$LIBFABRIC_ROOT/lib
+
+export GAM_CARDINALITY=2
+export GAM_RUN_LOCAL=$FAST_ROOT/bin/fastrun-local
+
+$FAST_ROOT/bin/fastrun-mpi -H hosts -n 2 $PWD/fast_mnist
