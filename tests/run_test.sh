0838e68cbc22fb25d4f6f84bc9e569c7eae7d735
Paolo Viviani <paolo.vivi@gmail.com>
2019-02-07 23:20:44 +0100

    sync

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -73,7 +73,8 @@ if [[ $LAUNCHER == localhost ]]; then
 	#$FAST_RUN_LOCAL -v -n 3 -l $GAM_LOCALHOST ./bin/gff_all_reduce_multi
 	#$FAST_RUN_LOCAL -v -n 3 -l $GAM_LOCALHOST ./bin/gff_all_reduce_vector
 	#$FAST_RUN_LOCAL -v -n 2 -l $GAM_LOCALHOST ./bin/mxnet_worker_test
-	$FAST_RUN_LOCAL -v -n 2 -l $GAM_LOCALHOST ./bin/gff_training_mockup
+	#$FAST_RUN_LOCAL -v -n 2 -l $GAM_LOCALHOST ./bin/gff_training_mockup
+	$FAST_RUN_LOCAL -v -n 2 -l $GAM_LOCALHOST ./bin/gff_training_concurrent
 elif [[ $LAUNCHER == mpi ]]; then
 	$FAST_RUN_MPI -H hosts -n 2 $PWD/bin/gam_unit_test
 	$FAST_RUN_MPI -H hosts -n 5 $PWD/bin/gff_farm
