4355112d8b96218f120e1f796102b3e90d94bddc
Paolo Viviani <paolo.vivi@gmail.com>
2019-02-19 00:45:42 +0100

    aync

--- examples/model_logics/resnet_cifar10.hpp
+++ examples/model_logics/resnet_cifar10.hpp
@@ -157,7 +157,7 @@ public:
 	MXDataIter train_iter = MXDataIter("ImageRecordIter");
 	Accuracy train_acc;
 	int batch_size_ = 32;
-	const int max_epoch_ = 200;
+	const int max_epoch_ = 50;
 	const std::string data_tag = "data";
 	const std::string label_tag = "label";
 };
