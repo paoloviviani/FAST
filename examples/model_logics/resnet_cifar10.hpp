5f46b04ba0e0f8b606075b88bf386988bf5c14d5
Paolo Viviani <paolo.vivi@gmail.com>
2019-02-20 11:39:48 +0100

    sync

--- examples/model_logics/resnet_cifar10.hpp
+++ examples/model_logics/resnet_cifar10.hpp
@@ -8,14 +8,23 @@ Context ctx = Context::cpu();  // Use CPU for training
 class ModelLogic {
 public:
 	void init() {
-		batch_size_ = 128;
+		if(const char* batch_env = std::getenv("BATCH_SIZE"))
+			batch_size_ = std::stoi(std::string(batch_env));
+		else
+			batch_size_ = 128;
+
+		std::string filename = "resnet50_worker_" + std::to_string(FAST::rank()) + "_batch_" + std::to_string(batch_size_) ;
+		log_file = ofstream(filename);
+
+		FAST_INFO("Batch size = " << batch_size_)
+		FAST_INFO("Number of nodes = " << FAST::cardinality())
+		assert(batch_size_ == 32 || batch_size_ == 64 || batch_size_ == 128 || batch_size_ == 256);
+
 		const int image_size = 32;
 		const float learning_rate = 0.01;
 		const float weight_decay = 1e-4;
 
 		net = Symbol::Load("../symbols/resnet50_v2.json");
-
-
 
 		Symbol label = Symbol::Variable("label");
 		net = SoftmaxOutput(net, label);
@@ -31,8 +40,20 @@ public:
 			.SetParam("batch_size", batch_size_)
 			.SetParam("shuffle", 1)
 			.SetParam("preprocess_threads", 24)
-//			.SetParam("num_parts", FAST::cardinality())
-//			.SetParam("part_index", FAST::rank())
+			.SetParam("num_parts", FAST::cardinality())
+			.SetParam("part_index", FAST::rank())
+			.CreateDataIter();
+
+		val_iter = MXDataIter("ImageRecordIter")
+			.SetParam("path_imglist", "../cifar10/cifar10_val.lst")
+			.SetParam("path_imgrec", "../cifar10/cifar10_val.rec")
+			.SetParam("rand_crop", 0)
+			.SetParam("rand_mirror", 0)
+			.SetParam("data_shape", Shape(3, 32, 32))
+			.SetParam("batch_size", batch_size_)
+			.SetParam("round_batch", 0)
+			.SetParam("preprocess_threads", 24)
+			.SetParam("pad", 2)
 			.CreateDataIter();
 
 		FAST_DEBUG("(LOGIC): Loaded data ");
@@ -50,7 +71,7 @@ public:
 //		}
 		// Load same weights for all the workers
 
-		args = mxnet::cpp::NDArray::LoadToMap("../initialized_weights/resnet50_cifar10_epoch0.bin");
+		args = mxnet::cpp::NDArray::LoadToMap("../initialized_weights/resnet50_cifar10_init"+std::to_string(batch_size_)+".bin");
 
 
 		opt = OptimizerRegistry::Find("adam");
@@ -60,20 +81,33 @@ public:
 		exec = net.SimpleBind(ctx, args);
 		arg_names = net.ListArguments();
 
+		log_file << "Epoch\tTime\tTraining accuracy\tTest accuracy" << std::endl;
+
 		FAST_DEBUG("Logic initialized")
 	}
 
-
 	void run_batch() {
-		FAST_DEBUG("(LOGIC): run batch, iteration = " << iter_);
 
 		if (!train_iter.Next()) {
 			FAST_DEBUG("(LOGIC): next epoch");
 			iter_ = 0;
 			epoch_++;
+
+			val_acc.Reset();
+			val_iter.Reset();
+			while (val_iter.Next()) {
+				auto data_batch = val_iter.GetDataBatch();
+				data_batch.data.CopyTo(&args["data"]);
+				data_batch.label.CopyTo(&args["label"]);
+				// Forward pass is enough as no gradient is needed when evaluating
+				exec->Forward(false);
+				val_acc.Update(data_batch.label, exec->outputs[0]);
+			}
 			NDArray::WaitAll();
-			std::cout << "=== Epoch === " << epoch_ << std::endl;
-			std::cout << "=== TRAINING ACCURACY === " << train_acc.Get() << std::endl;
+			std::cerr << "=== Epoch === " << epoch_ << std::endl;
+			std::cerr << "=== TRAINING ACCURACY === " << train_acc.Get() << std::endl;
+			std::cerr << "=== TEST ACCURACY === " << val_acc.Get() << std::endl;
+			log_file << epoch_ << "\t" << total_time << "\t" << train_acc.Get() << "\t\t" << acc.Get() << endl;
 			if (epoch_ == max_epoch_){
 				FAST_DEBUG("(LOGIC): MAX EPOCH REACHED");
 				max_epoch_reached = true; // Terminate
@@ -100,11 +134,7 @@ public:
 			if (arg_names[i] == "data" || arg_names[i] == "label") continue;
 			opt->Update(i, exec->arg_arrays[i], exec->grad_arrays[i]);
 		}
-		FAST_INFO("=======================================================");
-		FAST_INFO("Epoch = " << epoch_ << "Samples = " << iter_*batch_size_ );
-		FAST_INFO("Accuracy = " << train_acc.Get());
-		FAST_INFO("=======================================================");
-		FAST_DEBUG("(LOGIC): processed batch");
+		FAST_ERROR("Epoch = " << epoch_ << "Samples = " << iter_*batch_size_ );
 		iter_++;
 	}
 
@@ -120,19 +150,7 @@ public:
 	}
 
 	void finalize() {
-		auto val_iter = MXDataIter("ImageRecordIter")
-			.SetParam("path_imglist", "../cifar10/cifar10_val.lst")
-			.SetParam("path_imgrec", "../cifar10/cifar10_val.rec")
-			.SetParam("rand_crop", 0)
-			.SetParam("rand_mirror", 0)
-			.SetParam("data_shape", Shape(3, 32, 32))
-			.SetParam("batch_size", batch_size_)
-			.SetParam("round_batch", 0)
-			.SetParam("preprocess_threads", 24)
-			.SetParam("pad", 2)
-			.CreateDataIter();
-
-		Accuracy acc;
+		val_acc.Reset();
 		val_iter.Reset();
 		while (val_iter.Next()) {
 			auto data_batch = val_iter.GetDataBatch();
@@ -140,9 +158,10 @@ public:
 			data_batch.label.CopyTo(&args["label"]);
 			// Forward pass is enough as no gradient is needed when evaluating
 			exec->Forward(false);
-			acc.Update(data_batch.label, exec->outputs[0]);
+			val_acc.Update(data_batch.label, exec->outputs[0]);
 		}
 		NDArray::WaitAll();
+		MXNotifyShutdown();
 		FAST_INFO("=== VALIDATION ACCURACY === " << train_acc.Get());
 	}
 
@@ -155,9 +174,12 @@ public:
 	unsigned int epoch_ = 0;
 	bool max_epoch_reached = false;
 	MXDataIter train_iter = MXDataIter("ImageRecordIter");
-	Accuracy train_acc;
+	MXDataIter val_iter = MXDataIter("ImageRecordIter");
+	Accuracy train_acc, val_acc;
 	int batch_size_ = 32;
 	const int max_epoch_ = 1;
 	const std::string data_tag = "data";
 	const std::string label_tag = "label";
+	float total_time;
+	ofstream log_file;
 };
