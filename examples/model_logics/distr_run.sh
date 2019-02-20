471cb7c733676960434a4604af636d3a94cd9122
Paolo Viviani <paolo.vivi@gmail.com>
2019-02-21 00:31:28 +0100

    minor

--- examples/model_logics/distr_run.sh
+++ examples/model_logics/distr_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 FAST_ROOT=$(cd ../..; pwd)
-MXNET_LIBDIR=$FAST_ROOT/3rdparty/mxnet/lib
-LIBFABRIC_ROOT=/opt/libfabric
-#MXNET_LIBDIR=/home/pviviani/pviviani/opt/magnus/mxnet/lib
-#LIBFABRIC_ROOT=/home/pviviani/pviviani/opt/magnus/libfabric
+#MXNET_LIBDIR=$FAST_ROOT/3rdparty/mxnet/lib
+#LIBFABRIC_ROOT=/opt/libfabric
+MXNET_LIBDIR=/home/pviviani/pviviani/opt/magnus/mxnet/lib
+LIBFABRIC_ROOT=/home/pviviani/pviviani/opt/magnus/libfabric
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MXNET_LIBDIR:$LIBFABRIC_ROOT/lib
 
