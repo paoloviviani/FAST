648b23bc307089d108374036c3855710fb084490
Paolo Viviani <paolo.vivi@gmail.com>
2019-03-01 23:57:57 +0100

    test with out buffer

--- include/fast/workers/mxnet_worker.hpp
+++ include/fast/workers/mxnet_worker.hpp
@@ -19,6 +19,8 @@
 #include <ff/pipeline.hpp>
 #include <ff/node.hpp>
 
+#define RAW_PUBLIC_PAIR FAST::gam_vector<T> * , gam::public_ptr< FAST::gam_vector<T>
+
 namespace FAST {
 
 /**
@@ -33,12 +35,9 @@ constexpr auto TRIGGER_TOKEN = gff::go_on - 2;
 
 template<typename T>
 gam::public_ptr<T> token2public(uint64_t token) {
-	return gam::public_ptr<T>(gam::GlobalPointer(token));
+    return gam::public_ptr<T>(gam::GlobalPointer(token));
 }
 
-/**
- * Wrapper structs to pass objects to FF pipeline
- */
 typedef std::vector<mxnet::cpp::NDArray> NDAvector;
 
 /**
@@ -54,139 +53,139 @@ template <typename ModelLogic, typename T >
 class InputStage: public ff::ff_node {
 public:
 
-	InputStage(ModelLogic * logic) : logic_(logic), buffer_(nullptr), first_push_(true) {}
+    InputStage(ModelLogic * logic) : logic_(logic), buffer_(nullptr), first_push_(true) {}
 
-	void * svc(void * task) {
-		FAST_DEBUG("(INPUT STAGE): started svc")
+    void * svc(void * task) {
+        FAST_DEBUG("(INPUT STAGE): started svc");
 
-		if (task == NEXT_ITERATION) {
-			FAST_DEBUG("(INPUT STAGE): got trigger")
-			if (first_push_){
-				this->ff_send_out( NEXT_ITERATION );
-				first_push_ = false;
-			}
-			return ff::FF_GO_ON;
-		}
+        if (task == NEXT_ITERATION) {
+            FAST_DEBUG("(INPUT STAGE): got trigger");
+            if (first_push_){
+                this->ff_send_out( NEXT_ITERATION );
+                first_push_ = false;
+            }
+            return ff::FF_GO_ON;
+        }
 
-		auto recv_ptr = (FAST::gam_vector<float> *)task;
+        auto recv_ptr = (FAST::gam_vector<T> *)task;
 
-		FAST_DEBUG("(INPUT STAGE): got real pointer of size " << (*recv_ptr).size())
-		FAST::accumToNDVec( *recv_ptr, *buffer_, logic_->arg_names, logic_->data_tag, logic_->label_tag, 1., mxnet::cpp::Context::cpu() );
-//		delete recv_ptr;
-		gam::DELETE(recv_ptr);
+        FAST_DEBUG("(INPUT STAGE): got real pointer of size " << (*recv_ptr).size())
+        FAST::accumToNDVec( *recv_ptr, *buffer_, logic_->arg_names, logic_->data_tag, logic_->label_tag, 1., mxnet::cpp::Context::cpu() );
+        //		delete recv_ptr;
+        gam::DELETE(recv_ptr);
 
-		if (this->get_out_buffer()->empty()) {
-			FAST_DEBUG("(INPUT STAGE): push gradients")
-			this->ff_send_out((void *)buffer_);
-			buffer_ = new NDAvector(0);
-			FAST::buildNDVec( *buffer_, logic_->exec->grad_arrays, logic_->arg_names, mxnet::cpp::Context::cpu() );
-		}
-		return ff::FF_GO_ON;
-	}
+        if (this->get_out_buffer()->empty()) {
+            FAST_DEBUG("(INPUT STAGE): push gradients");
+            this->ff_send_out((void *)buffer_);
+            buffer_ = new NDAvector(0);
+            FAST::buildNDVec( *buffer_, logic_->exec->grad_arrays, logic_->arg_names, mxnet::cpp::Context::cpu() );
+        }
+        return ff::FF_GO_ON;
+    }
 
-	int svc_init() {
-		FAST_DEBUG("(INPUT STAGE): init stage")
-		buffer_ = new NDAvector(0);
-		FAST::buildNDVec( *buffer_, logic_->exec->grad_arrays, logic_->arg_names, mxnet::cpp::Context::cpu() );
-		FAST_DEBUG("(INPUT STAGE): Built NDVec");
-		return 0;
-	}
+    int svc_init() {
+        FAST_DEBUG("(INPUT STAGE): init stage");
+        buffer_ = new NDAvector(0);
+        FAST::buildNDVec( *buffer_, logic_->exec->grad_arrays, logic_->arg_names, mxnet::cpp::Context::cpu() );
+        FAST_DEBUG("(INPUT STAGE): Built NDVec");
+        return 0;
+    }
 
-	void svc_end() {
-		if(buffer_)
-			delete buffer_;
-	}
+    void svc_end() {
+        if(buffer_)
+            delete buffer_;
+    }
 private:
-	ModelLogic * logic_;
-	NDAvector * buffer_;
-	bool first_push_ = true;
+    ModelLogic * logic_;
+    NDAvector * buffer_;
+    bool first_push_ = true;
 };
 
 template <typename ModelLogic, typename T >
 class TrainerStage: public ff::ff_minode {
 public:
 
-	TrainerStage(ModelLogic * logic) : logic_(logic) {}
+    TrainerStage(ModelLogic * logic) : logic_(logic) {}
 
-	void * svc(void * task) {
-		FAST_DEBUG("(TRAINER STAGE): started svc");
+    void * svc(void * task) {
+        FAST_DEBUG("(TRAINER STAGE): started svc");
 
-		if (!logic_->max_epoch_reached) {
-			if (task != NEXT_ITERATION) {
-				// got a pointer from the input stage
-				FAST_DEBUG("(TRAINER STAGE): got gradients");
-				NDAvector * in_ptr = (NDAvector  *)task;
-				logic_->update( *in_ptr );
-				FAST_DEBUG("(TRAINER STAGE): executed batch from gradients");
-				delete in_ptr;
-			}
-			logic_->run_batch();
-			return NEXT_ITERATION;
-		}
-		else {
-			FAST_DEBUG("(TRAINER STAGE): returned end of input");
-			return END_OF_INPUT;
-		}
-		return ff::FF_GO_ON;
-	}
+        if (!logic_->max_epoch_reached) {
+            if (task != NEXT_ITERATION) {
+                // got a pointer from the input stage
+                FAST_DEBUG("(TRAINER STAGE): got gradients");
+                NDAvector * in_ptr = (NDAvector  *)task;
+                logic_->update( *in_ptr );
+                FAST_DEBUG("(TRAINER STAGE): executed batch from gradients");
+                delete in_ptr;
+            }
+            logic_->run_batch();
+            return NEXT_ITERATION;
+        }
+        else {
+            FAST_DEBUG("(TRAINER STAGE): returned end of input");
+            return END_OF_INPUT;
+        }
+        return ff::FF_GO_ON;
+    }
 private:
-	ModelLogic * logic_;
+    ModelLogic * logic_;
 
-	void eosnotify(ssize_t id) {
-		if (id == 0) {
-			// got EOS from input - forward TERMINATION_TAG message
-			this->ff_send_out(TERMINATION_TAG);
-		} else {
-			// got EOS from feedback - forward downstream to trigger termination
-			this->ff_send_out(ff::FF_EOS);
-			// got both EOSs - node will be terminated here
-		}
-	}
+    void eosnotify(ssize_t id) {
+        if (id == 0) {
+            // got EOS from input - forward TERMINATION_TAG message
+            this->ff_send_out(TERMINATION_TAG);
+        } else {
+            // got EOS from feedback - forward downstream to trigger termination
+            this->ff_send_out(ff::FF_EOS);
+            // got both EOSs - node will be terminated here
+        }
+    }
 };
 
 class internal_out_stage : public ff::ff_monode {
-	void *svc(void *in) {
-		if (in == TERMINATION_TAG){
-			// send EOS to the feedback channel
-			ff_send_out_to(ff::FF_EOS, 0);
-		}
-		else {
-			// send a NEXT_ITERATION message to the feedback channel
-			if (outnodes_[0]->get_out_buffer()->empty() && in != END_OF_INPUT)
-				ff_send_out_to(NEXT_ITERATION, 0);
-			// forward the input pointer downstream
-			ff_send_out_to(in, 1);
-		}
-		return ff::FF_GO_ON;
-	}
+    void *svc(void *in) {
+        if (in == TERMINATION_TAG){
+            // send EOS to the feedback channel
+            ff_send_out_to(ff::FF_EOS, 0);
+        }
+        else {
+            // send a NEXT_ITERATION message to the feedback channel
+            if (outnodes_[0]->get_out_buffer()->empty() && in != END_OF_INPUT)
+                ff_send_out_to(NEXT_ITERATION, 0);
+            // forward the input pointer downstream
+            ff_send_out_to(in, 1);
+        }
+        return ff::FF_GO_ON;
+    }
 
-	int svc_init() {
-		this->get_out_nodes(outnodes_);
-		return 0;
-	}
+    int svc_init() {
+        this->get_out_nodes(outnodes_);
+        return 0;
+    }
 private:
-	ff::svector<ff_node*> outnodes_;
+    ff::svector<ff_node*> outnodes_;
 };
 
 template <typename ModelLogic, typename T >
 class OutputStage: public ff::ff_node {
 public:
 
-	OutputStage(ModelLogic * logic) : logic_(logic){}
+    OutputStage(ModelLogic * logic) : logic_(logic){}
 
-	void * svc(void * task) {
-		if (task == END_OF_INPUT)
-			return END_OF_INPUT;
-		FAST_DEBUG("(OUTPUT STAGE): got pointer");
-		gam_vector<T> * out = new gam_vector<T>();
-		NDVecToVec( logic_->exec->grad_arrays, logic_->arg_names, *out, logic_->data_tag, logic_->label_tag);
-		FAST_DEBUG("(OUTPUT STAGE): allocated size " << out->size());
-		FAST_DEBUG("(OUTPUT STAGE): serialized gradients");
-		return (void*)out;
-	}
+    void * svc(void * task) {
+        if (task == END_OF_INPUT)
+            return END_OF_INPUT;
+        FAST_DEBUG("(OUTPUT STAGE): got pointer");
+        gam_vector<T> * out = new gam_vector<T>();
+        NDVecToVec( logic_->exec->grad_arrays, logic_->arg_names, *out, logic_->data_tag, logic_->label_tag);
+        FAST_DEBUG("(OUTPUT STAGE): allocated size " << out->size());
+        FAST_DEBUG("(OUTPUT STAGE): serialized gradients");
+        return (void*)out;
+    }
 
 private:
-	ModelLogic * logic_;
+    ModelLogic * logic_;
 };
 
 /**
@@ -196,97 +195,109 @@ private:
  */
 template< typename ModelLogic, typename T>
 class MXNetWorkerLogic {
+
+using public_raw_pair = std::pair< FAST::gam_vector<T> * , gam::public_ptr< FAST::gam_vector<T> > >;
+
 public:
 
-	gff::token_t svc(gam::public_ptr< gam_vector<T> > &in, gff::OutBundleBroadcast<gff::NondeterminateMerge> &c) {
-		// Check message and offload to pipe
-				switch(in.get().address()) {
-				case TRIGGER_TOKEN: {
-					pipe_->offload(NEXT_ITERATION);
-					break;
-				} // scope of 'x' ends here
-				case EOI_TOKEN: {
-					FAST_INFO("Received EOI token")
-					assert(eoi_cnt_ < ( neighbors ));
-					if (!eoi_out)
-						c.emit(token2public<FAST::gam_vector<float>>(EOI_TOKEN));
-					if(++eoi_cnt_ == neighbors)
-						return gff::eos;
-					return gff::go_on;
-				}
-				default: { //data
-					auto in_ptr = in.unique_local().release();
-					pipe_->offload( (void*)in_ptr );
-				}
-				}
+    gff::token_t svc(gam::public_ptr< gam_vector<T> > &in, gff::OutBundleBroadcast<gff::NondeterminateMerge> &c) {
+        // Check message and offload to pipe
+        switch(in.get().address()) {
+        case TRIGGER_TOKEN: {
+            pipe_->offload(NEXT_ITERATION);
+            break;
+        } // scope of 'x' ends here
+        case EOI_TOKEN: {
+            FAST_INFO("Received EOI token");
+            assert(eoi_cnt_ < ( neighbors ));
+            if (!eoi_out)
+                c.emit(token2public<FAST::gam_vector<T>>(EOI_TOKEN));
+            if(++eoi_cnt_ == neighbors)
+                return gff::eos;
+            return gff::go_on;
+        }
+        default: { //data
+            auto in_ptr = in.unique_local().release();
+            if (in_ptr->size() == 0)
+                return gff::go_on;
+            pipe_->offload( (void*)in_ptr );
+        }
+        }
 
-				void * outptr = nullptr;
-				while (!eoi_out) {
-					pipe_->load_result(&outptr);
-					if (outptr == END_OF_INPUT) {
-						FAST_DEBUG("(MXNET WORKER): Got EOI")
-						if (!eoi_out)
-							c.emit(token2public<FAST::gam_vector<float>>(EOI_TOKEN));
-						eoi_out = true;
-						return gff::go_on;
-					}
-					else { //out data
-						FAST_DEBUG("(MXNET WORKER): Got data")
-						FAST::gam_vector<float> * out_vec = (FAST::gam_vector<float> *)outptr;
-						auto out_ptr = gam::public_ptr< FAST::gam_vector<float> >(out_vec, [](FAST::gam_vector<float> * ptr){delete ptr;});
-						c.emit(out_ptr);
-						out_ptr.reset();
-						return gff::go_on;
-					}
-				}
-				return gff::go_on;
-	}
+        void * outptr = nullptr;
+        while (!eoi_out) {
+            pipe_->load_result(&outptr);
+            if (outptr == END_OF_INPUT) {
+                FAST_DEBUG("(MXNET WORKER): Got EOI");
+                if (!eoi_out)
+                    c.emit(token2public<FAST::gam_vector<T>>(EOI_TOKEN));
+                eoi_out = true;
+                return gff::go_on;
+            }
+            else { //out data
+                FAST_DEBUG("(MXNET WORKER): Got data");
+                FAST::gam_vector<T> * out_vec = (FAST::gam_vector<T> *)outptr;
+                auto out_ptr = gam::public_ptr< FAST::gam_vector<T> >(out_vec, [](FAST::gam_vector<T> * ptr){delete ptr;});
 
-	void svc_init(gff::OutBundleBroadcast<gff::NondeterminateMerge> &c) {
+                out_buffer_.push(std::make_pair(out_vec, out_ptr));
 
-		neighbors = c.internals.out_cardinality();
+                c.emit(out_buffer_.back().second);
 
-		pipe_ = new ff::ff_pipeline(true);
-		training_ = new ff::ff_pipeline();
+                if (out_buffer_.size() == 4) {
+                    out_buffer_.back().first->resize(0);
+                    out_buffer_.back().second.reset();
+                }
+                return gff::go_on;
+            }
+        }
+        return gff::go_on;
+    }
 
-		FAST_DEBUG("(MXNET WORKER): Initializing model logic")
-		logic_.init();
-		FAST_DEBUG("(MXNET WORKER): Initialized model logic")
+    void svc_init(gff::OutBundleBroadcast<gff::NondeterminateMerge> &c) {
 
-		FAST_DEBUG("(MXNET WORKER): Creating pipeline")
-		pipe_->add_stage( new InputStage<ModelLogic, T>(&logic_) );
-		training_->add_stage( new TrainerStage<ModelLogic, T>(&logic_) );
-		training_->add_stage( new internal_out_stage() );
-		training_->wrap_around();
-		pipe_->add_stage(training_);
-		pipe_->add_stage( new OutputStage<ModelLogic, T>(&logic_) );
+        neighbors = c.internals.out_cardinality();
 
-		pipe_->cleanup_nodes();
-		training_->cleanup_nodes();
+        pipe_ = new ff::ff_pipeline(true);
+        training_ = new ff::ff_pipeline();
 
-		FAST_DEBUG("(MXNET WORKER): Launching pipeline")
-		pipe_->run();
+        FAST_DEBUG("(MXNET WORKER): Initializing model logic");
+        logic_.init();
+        FAST_DEBUG("(MXNET WORKER): Initialized model logic");
 
-		FAST_DEBUG("(MXNET WORKER): Emitting trigger")
-		c.emit(token2public<FAST::gam_vector<float>>(TRIGGER_TOKEN));
-	}
+        FAST_DEBUG("(MXNET WORKER): Creating pipeline")
+        pipe_->add_stage( new InputStage<ModelLogic, T>(&logic_) );
+        training_->add_stage( new TrainerStage<ModelLogic, T>(&logic_) );
+        training_->add_stage( new internal_out_stage() );
+        training_->wrap_around();
+        pipe_->add_stage(training_);
+        pipe_->add_stage( new OutputStage<ModelLogic, T>(&logic_) );
 
-	void svc_end(gff::OutBundleBroadcast<gff::NondeterminateMerge> &c) {
-		FAST_DEBUG("(FINALIZATION)");
-		pipe_->offload( ff::FF_EOS );
-		if (pipe_->wait()<0) {
-			FAST_DEBUG("(FINALIZATION): error waiting pipe");
-		}
-		logic_.finalize();
-	}
+        pipe_->cleanup_nodes();
+        training_->cleanup_nodes();
+
+        FAST_DEBUG("(MXNET WORKER): Launching pipeline");
+        pipe_->run();
+
+        FAST_DEBUG("(MXNET WORKER): Emitting trigger");
+        c.emit(token2public<FAST::gam_vector<T>>(TRIGGER_TOKEN));
+    }
+
+    void svc_end(gff::OutBundleBroadcast<gff::NondeterminateMerge> &c) {
+        FAST_DEBUG("(FINALIZATION)");
+        pipe_->offload( ff::FF_EOS );
+        if (pipe_->wait()<0) {
+            FAST_DEBUG("(FINALIZATION): error waiting pipe");
+        }
+        logic_.finalize();
+    }
 private:
-	ff::ff_pipeline * pipe_;
-	ff::ff_pipeline * training_;
-	ModelLogic logic_;
-	int eoi_cnt_ = 0;
-	bool eoi_out = false;
-	int neighbors;
-	std::vector< gam::public_ptr< FAST::gam_vector<float> > > out_buffer_;
+    ff::ff_pipeline * pipe_;
+    ff::ff_pipeline * training_;
+    ModelLogic logic_;
+    int eoi_cnt_ = 0;
+    bool eoi_out = false;
+    int neighbors;
+    std::queue< std::pair< RAW_PUBLIC_PAIR > > > out_buffer_;
 };
 
 } // namespace FAST
--- tests/mxnet_test/mxnet_worker_test.cpp
+++ tests/mxnet_test/mxnet_worker_test.cpp
@@ -25,67 +25,67 @@ using namespace mxnet::cpp;
 Context ctx = Context::cpu();  // Use CPU for training
 
 struct Dummy {
-	std::vector<NDArray> grad_arrays;
+  std::vector<NDArray> grad_arrays;
 };
 
 class ModelLogic {
 public:
-	void init() {
-		Context ctx = Context::cpu();  // Use CPU for training
+  void init() {
+    Context ctx = Context::cpu();  // Use CPU for training
 
-		exec = new Dummy();
-		arg_names.push_back("first");
-		arg_names.push_back("second");
-		arg_names.push_back("third");
-		arg_names.push_back("fourth");
+    exec = new Dummy();
+    arg_names.push_back("first");
+    arg_names.push_back("second");
+    arg_names.push_back("third");
+    arg_names.push_back("fourth");
 
-		for (size_t i = 0; i < arg_names.size(); ++i) {
-			exec->grad_arrays.push_back( NDArray(Shape(BATCH_SIZE, 100), ctx) );
-			exec->grad_arrays[i] = 0.;
-//			FAST_DEBUG("(LOGIC INIT): gradients initial values = " << exec->grad_arrays[i])
-		}
-		FAST_INFO("Logic initialized")
-	}
+    for (size_t i = 0; i < arg_names.size(); ++i) {
+      exec->grad_arrays.push_back( NDArray(Shape(BATCH_SIZE, 100), ctx) );
+      exec->grad_arrays[i] = 0.;
+      //			FAST_DEBUG("(LOGIC INIT): gradients initial values = " << exec->grad_arrays[i])
+    }
+    FAST_INFO("Logic initialized")
+  }
 
 
-	void run_batch() {
-		FAST_INFO("(LOGIC): run batch, iteration = " << iter_)
-		for (size_t i = 0; i < arg_names.size(); ++i) {
-			exec->grad_arrays[i] += 0.1;
-//			FAST_DEBUG("(LOGIC RUN): gradients new values = " << exec->grad_arrays[i])
-			std::this_thread::sleep_for(std::chrono::milliseconds(200));
-		}
-		iter_++;
-		if (iter_ == 5000)
-			max_epoch_reached = true; // Terminate
-	}
+  void run_batch() {
+    FAST_INFO("(LOGIC): run batch, iteration = " << iter_)
+		    for (size_t i = 0; i < arg_names.size(); ++i) {
+		      exec->grad_arrays[i] += 0.1;
+		      //			FAST_DEBUG("(LOGIC RUN): gradients new values = " << exec->grad_arrays[i])
+		      std::this_thread::sleep_for(std::chrono::milliseconds(200));
+		    }
+    iter_++;
+    if (iter_ == 5000)
+      max_epoch_reached = true; // Terminate
+  }
 
-	void update(std::vector<mxnet::cpp::NDArray> &in) {
-		REQUIRE(in.size() > 0);
-		for (size_t i = 0; i < arg_names.size(); ++i) {
-//			FAST_DEBUG("(LOGIC UPDATE): original gradients = " << exec->grad_arrays[i])
-//			FAST_DEBUG("(LOGIC UPDATE): incoming gradients = " << in[i])
-			exec->grad_arrays[i] += in[i];
-//			FAST_DEBUG("(LOGIC UPDATE): updated gradients values = " << exec->grad_arrays[i])
-		}
-		FAST_INFO("(LOGIC UPDATE): updated")
-	}
+  void update(std::vector<mxnet::cpp::NDArray> &in) {
+    REQUIRE(in.size() > 0);
+    for (size_t i = 0; i < arg_names.size(); ++i) {
+      //			FAST_DEBUG("(LOGIC UPDATE): original gradients = " << exec->grad_arrays[i])
+      //			FAST_DEBUG("(LOGIC UPDATE): incoming gradients = " << in[i])
+      exec->grad_arrays[i] += in[i];
+      //			FAST_DEBUG("(LOGIC UPDATE): updated gradients values = " << exec->grad_arrays[i])
+    }
+    FAST_INFO("(LOGIC UPDATE): updated");
+  }
 
-	void finalize() {
+  void finalize() {
 
-	}
+  }
 
-	Dummy * exec;
-	vector<string> arg_names;
-	unsigned int iter_ = 0;
-	bool max_epoch_reached = false;
-	const std::string data_tag = "X";
-	const std::string label_tag = "label";
+  Dummy * exec;
+  vector<string> arg_names;
+  unsigned int iter_ = 0;
+  bool max_epoch_reached = false;
+  const std::string data_tag = "X";
+  const std::string label_tag = "label";
 };
 
 typedef gff::Filter<gff::NondeterminateMerge, gff::OutBundleBroadcast<gff::NondeterminateMerge>,//
-		gam::public_ptr< FAST::gam_vector<float> >, gam::public_ptr< FAST::gam_vector<float> >, //
-		FAST::MXNetWorkerLogic<ModelLogic, float> > MxNetWorker;
+    gam::public_ptr< FAST::gam_vector<float> >, gam::public_ptr< FAST::gam_vector<float> >, //
+    FAST::MXNetWorkerLogic<ModelLogic, float> > MxNetWorker;
 
 /*
  *******************************************************************************
@@ -96,19 +96,19 @@ typedef gff::Filter<gff::NondeterminateMerge, gff::OutBundleBroadcast<gff::Nonde
  */
 
 TEST_CASE( "MxNet worker basic test", "gam,gff,multi,mxnet" ) {
-	FAST_LOG_INIT
-	FAST_INFO("TEST name: "<< Catch::getResultCapture().getCurrentTestName());
+  FAST_LOG_INIT
+  FAST_INFO("TEST name: "<< Catch::getResultCapture().getCurrentTestName());
 
-	gff::NondeterminateMerge to_one, to_two;
-	gff::OutBundleBroadcast<gff::NondeterminateMerge> one, two;
+  gff::NondeterminateMerge to_one, to_two;
+  gff::OutBundleBroadcast<gff::NondeterminateMerge> one, two;
 
-	one.add_comm(to_two);
-	two.add_comm(to_one);
+  one.add_comm(to_two);
+  two.add_comm(to_one);
 
-	gff::add(MxNetWorker(to_one,one));
-	gff::add(MxNetWorker(to_two,two));
+  gff::add(MxNetWorker(to_one,one));
+  gff::add(MxNetWorker(to_two,two));
 
-	/* execute the network */
-	gff::run();
+  /* execute the network */
+  gff::run();
 
 }
