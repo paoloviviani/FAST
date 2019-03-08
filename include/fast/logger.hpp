8bf722a06ea15c6a29a989ca9b4b7e2c921fbdd9
Paolo Viviani <paolo.vivi@gmail.com>
2019-03-08 13:52:17 +0100

    synv

--- include/fast/logger.hpp
+++ include/fast/logger.hpp
@@ -22,67 +22,95 @@
 #define FAST_LOG_INIT FAST::Logger::getLogger()->init();
 #define FAST_LOG_FINALIZE FAST::Logger::getLogger()->finalize();
 
-
 #if defined LOGLEVEL_DEBUG
-#define FAST_DEBUG(x) {\
-		FAST::Logger::getLogger()->lock(); \
-		FAST::Logger::getLogger()->log_debug() << x << std::endl << std::flush; \
-		FAST::Logger::getLogger()->unlock();}
-#define FAST_INFO(x) {\
-		FAST::Logger::getLogger()->lock(); \
-		FAST::Logger::getLogger()->log_info() << x << std::endl << std::flush; \
-		FAST::Logger::getLogger()->unlock();}
-#define FAST_ERROR(x) {\
-		FAST::Logger::getLogger()->lock(); \
-		FAST::Logger::getLogger()->log_error() << x << std::endl << std::flush; \
-		FAST::Logger::getLogger()->unlock();}
+#define FAST_DEBUG(x)                                            \
+	{                                                            \
+		FAST::Logger::getLogger()->lock();                       \
+		FAST::Logger::getLogger()->log_debug() << x << std::endl \
+											   << std::flush;    \
+		FAST::Logger::getLogger()->unlock();                     \
+	}
+#define FAST_INFO(x)                                            \
+	{                                                           \
+		FAST::Logger::getLogger()->lock();                      \
+		FAST::Logger::getLogger()->log_info() << x << std::endl \
+											  << std::flush;    \
+		FAST::Logger::getLogger()->unlock();                    \
+	}
+#define FAST_ERROR(x)                                            \
+	{                                                            \
+		FAST::Logger::getLogger()->lock();                       \
+		FAST::Logger::getLogger()->log_error() << x << std::endl \
+											   << std::flush;    \
+		FAST::Logger::getLogger()->unlock();                     \
+	}
 
 #elif defined LOGLEVEL_INFO
-#define FAST_DEBUG(...) {}
-#define FAST_INFO(x) {\
-		FAST::Logger::getLogger()->lock(); \
+#define FAST_DEBUG(...) \
+	{                   \
+	}
+#define FAST_INFO(x)                                             \
+	{                                                            \
+		FAST::Logger::getLogger()->lock();                       \
 		FAST::Logger::getLogger()->log_info() << x << std::endl; \
-		FAST::Logger::getLogger()->unlock();}
-#define FAST_ERROR(x) {\
-		FAST::Logger::getLogger()->lock(); \
-		FAST::Logger::getLogger()->log_error() << x << std::endl << std::flush; \
-		FAST::Logger::getLogger()->unlock();}
+		FAST::Logger::getLogger()->unlock();                     \
+	}
+#define FAST_ERROR(x)                                            \
+	{                                                            \
+		FAST::Logger::getLogger()->lock();                       \
+		FAST::Logger::getLogger()->log_error() << x << std::endl \
+											   << std::flush;    \
+		FAST::Logger::getLogger()->unlock();                     \
+	}
 
 #else
-#define FAST_DEBUG(...) {}
-#define FAST_INFO(...) {}
-#define FAST_ERROR(x) {\
-		FAST::Logger::getLogger()->lock(); \
-		FAST::Logger::getLogger()->log_error() << x << std::endl << std::flush; \
-		FAST::Logger::getLogger()->unlock();}
+#define FAST_DEBUG(...) \
+	{                   \
+	}
+#define FAST_INFO(...) \
+	{                  \
+	}
+#define FAST_ERROR(x)                                            \
+	{                                                            \
+		FAST::Logger::getLogger()->lock();                       \
+		FAST::Logger::getLogger()->log_error() << x << std::endl \
+											   << std::flush;    \
+		FAST::Logger::getLogger()->unlock();                     \
+	}
 
 #endif
 
 // Get current date/time, format is YYYY-MM-DD.HH:mm:ss
-inline const std::string currentDateTime() {
-	time_t     now = time(0);
-	struct tm  tstruct;
-	char       buf[80];
+inline const std::string currentDateTime()
+{
+	time_t now = time(0);
+	struct tm tstruct;
+	char buf[80];
 	tstruct = *localtime(&now);
 	strftime(buf, sizeof(buf), "%X", &tstruct);
 	return buf;
 }
 
-namespace FAST {
+namespace FAST
+{
 
-class Logger {
-public:
-	static Logger *getLogger() {
+class Logger
+{
+  public:
+	static Logger *getLogger()
+	{
 		static Logger logger;
 		return &logger;
 	}
 
-	void init() {
+	void init()
+	{
 		id = FAST::rank();
 		log("I am FAST worker %d", id);
 	}
 
-	void finalize() {
+	void finalize()
+	{
 		//print footer message
 		log("stop logging worker %d", id);
 	}
@@ -91,31 +119,35 @@ public:
 	 * Standard error stream provider
 	 * @return
 	 */
-	std::ostream &log_error() {
-		return std::cerr << " (FAST ERROR) [" << currentDateTime() << ", proc " << id <<"] === ";
+	std::ostream &log_error()
+	{
+		return std::cerr << " (FAST ERROR) [" << currentDateTime() << ", proc " << id << "] === \t";
 	}
 
 	/**
 	 * Standard output stream provider
 	 * @return
 	 */
-	std::ostream &log_info() {
-		return std::cout << " (FAST INFO) [" << currentDateTime() << ", proc " << id <<"] === ";
+	std::ostream &log_info()
+	{
+		return std::cout << " (FAST INFO) [" << currentDateTime() << ", proc " << id << "] === \t";
 	}
 
 	/**
 	 * Standard output stream provider
 	 * @return
 	 */
-	std::ostream &log_debug() {
-		return std::cout << " (FAST DEBUG) [" << currentDateTime() << ", proc " << id <<"] === ";
+	std::ostream &log_debug()
+	{
+		return std::cout << " (FAST DEBUG) [" << currentDateTime() << ", proc " << id << "] === \t";
 	}
 
 	/**
 	 *   Variable Length Logger function
 	 *   @param format string for the message to be logged.
 	 */
-	void log(const char * format, ...) {
+	void log(const char *format, ...)
+	{
 		//print message
 		va_start(args, format);
 		vsprintf(sMessage, format, args);
@@ -125,21 +157,22 @@ public:
 		va_end(args);
 	}
 
-	void lock() {
+	void lock()
+	{
 		mtx.lock();
 	}
 
-	void unlock() {
+	void unlock()
+	{
 		mtx.unlock();
 	}
 
-private:
+  private:
 	uint32_t id;
 	std::mutex mtx;
 
 	char sMessage[256];
 	va_list args;
-
 };
 
 } //Namespace FAST
